b5175f05ee987ba078ebc05d77692fa6d19bbf10
QQ GG <esandier@yahoo.fr>
2022-05-13 15:22:12 +0200

    commit 2

--- surface_play/views.py
+++ surface_play/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# pages/views.py
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse("Salut les nazes !")
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
--- surfaces_project/settings.py
+++ surfaces_project/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'surfaces_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                    BASE_DIR / "templates",  # new
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
